253b79224489793a869f3c1cb73c5df0bc7ccccc
TheKorn01 <101316213+TheKorn01@users.noreply.github.com>
2023-11-23 14:51:02 +0700

    V.1.3-bear

--- src/App.tsx
+++ src/App.tsx
@@ -10,20 +10,28 @@ function App() {
 
   return (
     <>
-    <TE/>
-    <div className="w-fit h-screen bg-[#ffffff]">
-      
-    
-    <Boxbear icon='ic:round-dashboard' TBOXT='Dashboard' />
-    <Boxbear icon='radix-icons:dashboard' TBOXT='UI-Element' />
-    <Boxbear icon='wpf:books' TBOXT='Books Store' />
-    <Boxbear icon='fluent:book-add-24-filled' TBOXT='Add books' />
-    <Boxbear icon='material-symbols:edit' TBOXT='Edit books' />
-    <Boxbear icon='fluent:people-community-16-filled' TBOXT='Community' />
-    <Boxbear icon='majesticons:comment' TBOXT='Comment' />
-    <Boxbear icon='ic:round-dashboard' TBOXT='User Data' />
-    <Boxbear icon='tdesign:setting-1' TBOXT='Setting' />
-  </div> 
+      <TE />
+      <div className='flex'>
+        <div className="w-fit h-screen bg-[#ffffff]">
+          <Boxbear icon='ic:round-dashboard' TBOXT='Dashboard' />
+          <Boxbear icon='radix-icons:dashboard' TBOXT='UI-Element' />
+          <Boxbear icon='wpf:books' TBOXT='Books Store' />
+          <Boxbear icon='fluent:book-add-24-filled' TBOXT='Add books' />
+          <Boxbear icon='material-symbols:edit' TBOXT='Edit books' />
+          <Boxbear icon='fluent:people-community-16-filled' TBOXT='Community' />
+          <Boxbear icon='majesticons:comment' TBOXT='Comment' />
+          <Boxbear icon='ic:round-dashboard' TBOXT='User Data' />
+          <Boxbear icon='tdesign:setting-1' TBOXT='Setting' />
+        </div>
+        <div className='flex flex-1 flex-col p-3 gap-3'>
+
+          <div className='flex flex-1 gap-3'>
+            <div className='bg-white flex-[2] rounded-xl' />
+            <div className='bg-white flex-1 rounded-xl' />
+          </div> 
+          <div className='flex-1 bg-white rounded-xl'></div>
+        </div>
+      </div>
     </>
   )
 }
--- src/sibear/T.tsx
+++ src/sibear/T.tsx
@@ -1,20 +1,23 @@
-import React from 'react'
+
 import { Icon } from '@iconify/react';
 
 function T() {
   return (
     <div className=' justify-start text-[20px] '>
       <div className='flex'>
-        <div className='px-[20px] bg-[rgb(71,181,255)] w-[224px] py-[15px] text-white font-bold'>READ SOCIETY</div>
-        <div className='bg-[#ffffff] w-full ' >
-        <div className='flex my-[13px] w-full justify-end '>
-          <Icon icon="bxs:message" color="#7a7a7a" width="30" height="30" />
-          <Icon icon="mdi:bell" color="#7a7a7a" width="30" height="30" />
+        <div className='px-[20px] bg-[rgb(71,181,255)] w-[201px] h-[60px] py-[15px]'>
+          <img src="RD_white.png" width="130" className='mt-[-9px] ml-[10px] ' />
+
+        </div>
+        <div className='bg-[#ffffff] flex-1 ' >
+        <div className='flex my-[13px] justify-end '>
+          <Icon icon="bxs:message" color="#7a7a7a" width="30" height="30" className='mr-[20px]' />
+          <Icon icon="mdi:bell" color="#7a7a7a" width="30" height="30" className='mr-[20px]' />
           <div className='flex mx-[10px]'>
-            <p>OK</p>
-            <div className='bg-black h-[35px] w-[35px] ml-[10px] rounded-full'></div>
+            <p className='mx-[10px]'>Will Smith</p>
+            <img src="https://www.techhub.in.th/wp-content/uploads/2021/05/577280151-1.jpg" alt="MeMe" className=' h-[35px] w-[35px] ml-[10px] rounded-full' />
           </div>
-          <Icon icon="mingcute:menu-fill" color="#7a7a7a" width="30" height="30" />
+          <Icon icon="mingcute:menu-fill" color="#7a7a7a" width="30" height="30" className='mx-[20px]' />
         </div>
         
       </div>
